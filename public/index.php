ce0268306b81e9c5c5d454974bf3c528b7e92730
micos7 <micos7@gmail.com>
2018-07-25 08:27:43 +0300

    Db

--- public/index.php
+++ public/index.php
@@ -3,11 +3,18 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
+require '../includes/DbConnect.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
+
+    $db = new DbConnect();
+
+    if($db->connect() != null){
+        echo "Connected";
+    }
 
     return $response;
 });
