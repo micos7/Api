77bafc2f9bf3ec65c770ac3394832c3a0bca8e0c
micos7 <micos7@gmail.com>
2018-07-26 08:31:10 +0300

    Some db opeerations

---
+++ includes/DbOperations.php
@@ -0,0 +1,35 @@
+<?php
+class DbOperations {
+    private $con;
+
+    function __construct(){
+        require_once dirname(__FILE__). "DbConnect.php";
+        $db = new DbConnect();
+        $this->con = $db->connect();
+    }
+
+    public function createUser($email,$password,$name,$school){
+        if(!isEmailExist($email)){
+            $stmt = $this->con->prepare("INSERT INTO users(email,password,name,school) VALUES(?,?,?,?)");
+            $stmt->bind_param("ssss",$email,$password,$name,$school);
+            if($stmt->execute()){
+                return USER_CREATED;
+            }
+                return USER_FAILURE;
+        }
+        return USER_EXISTS;
+    }
+
+    private function isEmailExist($email){
+        $stmt = $this->con->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->bind_param("s",$email);
+        $stmt->execute();
+        $stmt->store_result();
+
+        return $stmt->num_rows > 0;
+    }
+}
+
+
+
+?>
