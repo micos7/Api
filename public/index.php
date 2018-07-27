47280a61b6362fb98335a33727f677d8fcce2341
micos7 <micos7@gmail.com>
2018-07-27 07:46:11 +0300

    create user

--- public/index.php
+++ public/index.php
@@ -3,19 +3,62 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require '../includes/DbConnect.php';
+require '../includes/DbOperations.php';
 
 $app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
 
-    $db = new DbConnect();
+/*
+endpoint: createuser
+parameters: email,password,name,school
+method: POST
+*/
 
-    if($db->connect() != null){
-        echo "Connected";
+$app->post('/createuser', function(Request $request,Response $response){
+    if(haveEmptyParameters(array('email','password','name','school'))){
+        $request_data = $request->getParsedBody();
+
+        $email = $request_data['email'];
+        $password = $request_data['password'];
+        $name = $request_data['name'];
+        $school = $request_data['school'];
+
+        $hash_password = password_hash($password, PASSWORD_DEFAULT);
+
+        $db = new DbOperations;
+
+        $result = $dn->createUser($email,$hash_password,$name,$school);
+
+        if($result == USER_CREATED){
+
+        }else if($result == USER_FAILURE) {
+
+        }else if($result == USER_EXISTS) {
+
+        }
+    }
+});
+
+function haveEmptyParameters($required_params,$response){
+    $error = false;
+    $error_params= '';
+    $request_params = $_REQUEST;
+
+    foreach ($request_params as $param) {
+        if(!isset($request_params[$param]) || strlen($request_params[$param])<= 0){
+            $error = true;
+            $error_params .= $param. ' ,';
+        }
     }
 
-    return $response;
-});
+    if($error){
+        $error_detail = array();
+        $error_detail['error'] = true;
+        $error_detail['message'] = 'Required parameters '. substr($error_params, 0 , -2). ' are missing or empty';
+        $response->write(json_encode($error_detail));
+    }
+
+    return $error;
+
+    }
+
 $app->run();
