8ae923add22c6b2a1c990d56ee9e11335cf6291c
micos7 <micos7@gmail.com>
2018-07-30 08:23:00 +0300

    update password endpoint

--- public/index.php
+++ public/index.php
@@ -186,6 +186,52 @@ $app->put('/updateuser/{id}', function(Request $request,Response $response, arra
             ->withStatus(200);
 });
 
+$app->put('/updatepassword', function(Request $request,Response $response){
+
+    if(!haveEmptyParameters(array('currentpassword','newpassword','email'), $request, $response)){
+        $request_data = $request->getParsedBody();
+
+        $currentpassword = $request_data['currentpassword'];
+        $newpassword = $request_data['newpassword'];
+        $email = $request_data['email'];
+
+        $db = new DbOperations;
+
+        $result = $db->updatePassword($currentpassword, $newpassword, $email);
+
+        if($result == PASSWORD_CHANGED){
+            $response_data = array();
+            $response_data['error'] = false;
+            $response_data['message'] = 'Password updated';
+            $response->write(json_encode($response_data));
+
+            return $response->withHeader("Content-Type","application/json")
+            ->withStatus(200);
+
+        }else if($result == PASSWORD_DO_NOT_MATCH){
+            $response_data = array();
+            $response_data['error'] = true;
+            $response_data['message'] = 'Password does not match!';
+            $response->write(json_encode($response_data));
+
+            return $response->withHeader("Content-Type","application/json")
+            ->withStatus(200);
+
+        }else if($result == PASSWORD_NOT_CHANGED){
+            $response_data = array();
+            $response_data['error'] = true;
+            $response_data['message'] = 'Try again later!';
+            $response->write(json_encode($response_data));
+
+            return $response->withHeader("Content-Type","application/json")
+            ->withStatus(200);
+        }
+    }
+    return $response->withHeader("Content-Type","application/json")
+            ->withStatus(422);
+});
+
+
 
 function haveEmptyParameters($required_params, $request, $response){
     $error = false;
